0c627d2ba6a860df986386701c11d95102ec9d00
ihkaru <ihza2karunia@gmail.com>
2023-04-30 19:24:40 +0700

    fix artisan controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,11 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
-    
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function responsejson($code, $data, $message = '')
+    {
+        $status = ($code >= 200 && $code < 300) ? true : false;
+        if ($message == '') {
+            return response()->json([
+                "status" => $status,
+                "code" => $code,
+                "data" => $data
+            ], $code);
+        } else {
+            return response()->json([
+                "status" => $status,
+                "code" => $code,
+                "data" => $data,
+                "message" => $message
+            ], $code);
+        }
+    }
+
+    public function responseabort($code, $message)
+    {
+        return $this->responsejson($code, ["message" => $message]);
+    }
+
+    public function emptyHandler(String $key)
+    {
+        if (request($key)) {
+            return request($key);
+        } else {
+            if (request()->exists($key)) {
+                return "";
+            } else {
+                return null;
+            }
+        }
+    }
 }
